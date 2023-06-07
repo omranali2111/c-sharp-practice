657b956449a7ccc78c514c74566a02f19c4c5227
omranali2111 <aljabri1234@hotmail.com>
2023-06-07 11:08:29 +0400

    practice two (months)

--- c-sharp-practice/Program.cs
+++ c-sharp-practice/Program.cs
@@ -26,6 +26,53 @@ internal class Program
 
         }
 
+        int month;
 
+        WriteLine("enter number of a month(1-12): ");
+        month= int.Parse(ReadLine());
+        switch (month)
+        {
+            case 1:
+                WriteLine("January");
+                break;
+            case 2:
+                WriteLine("February");
+                break;
+            case 3:
+                WriteLine(" March");
+                break;
+            case 4:
+                WriteLine("April");
+                break;
+            case 5:
+               WriteLine("May");
+                break;
+            case 6:
+                WriteLine("June");
+                break;
+            case 7:
+                WriteLine("July");
+                break;
+            case 8:
+                WriteLine("August");
+                break;
+            case 9:
+                WriteLine(" September");
+                break;
+            case 10:
+                WriteLine("October");
+                break;
+            case 11:
+                WriteLine("November");
+                break;
+            case 12:
+                WriteLine("December");
+                break;
+            default:
+                WriteLine("You have entered invalid number");
+                break;
+        }
     }
+
+
 }

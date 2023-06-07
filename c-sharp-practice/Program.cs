67cde6dbcb1f8f72cac878a6368f51234dc8a5c1
omranali2111 <aljabri1234@hotmail.com>
2023-06-07 10:50:08 +0400

    switch case  practice

---
+++ c-sharp-practice/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using static System.Console;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char a, e, i, o, u, chr;
+
+        WriteLine("enter a char: ");
+        chr=(ReadLine()[0]);
+
+        switch (chr)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                    WriteLine("this is Vowel letter");
+                break;
+
+            default:
+                WriteLine("this is Consonant letter");
+                break;
+
+        }
+
+
+    }
+}
